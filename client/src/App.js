84a43184d42ada70ae8781fb3f19ef17f2498594
Rko_8001 <yadwindersingh0705@gmail.com>
2023-05-04 19:17:35 +0530

    Added Error Page and Protected Admin Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import NavBar from './components_shared/navBars/NavBar';
 import LandingPage from './components_shared/sidePages/LandingPage';
-import Team from './components_shared/sidePages/teams/Team';
 import { Routes, Route } from 'react-router-dom';
 import SignIn from './components_login/SignIn';
 import SignUp from './components_login/SignUp';
 
 
+import StudentRoute from './routes/StudentRoutes';
 import {
   StudentHome, Profiles,
   ViewProblem, ViewSpecficProblem,
@@ -14,20 +14,16 @@ import {
   AddBlogs, AddProblems, AddEditorials,
   AdminHome
 } from './routes/Routes';
+import AdminRoutes from './routes/AdminRoutes';
 
+import ErrorPage from './components_shared/errorPages/ErrorPage';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<LandingPage />} />
-        <Route path='/meetTheTeam'
-          element={
-            <>
-              <NavBar />
-              <Team />
-            </>
-          } />
+
         <Route path='/signin'
           element={
             <>
@@ -45,17 +41,17 @@ function App() {
           } />
 
         <Route path='/admin'>
-          <Route index element={AdminHome} />
-          <Route path='profile' element={Profiles} />
-          <Route path='addBlog' element={AddBlogs} />
-          <Route path='addProblem' element={AddProblems} />
-          <Route path='addEditorial/:id' element={AddEditorials} />
+          <Route index element={<AdminRoutes Component={AdminHome} />} />
+          <Route path='profile' element={<AdminRoutes Component={Profiles} />} />
+          <Route path='addBlog' element={<AdminRoutes Component={AddBlogs} />} />
+          <Route path='addProblem' element={<AdminRoutes Component={AddProblems} />} />
+          <Route path='addEditorial/:id' element={<AdminRoutes Component={AddEditorials} />} />
         </Route>
 
 
         <Route path='/student'>
-          <Route index element={StudentHome} />
-          <Route path='profile' element={Profiles} />
+          <Route index element={<StudentRoute Component={StudentHome} />} />
+          <Route path='profile' element={<StudentRoute Component={Profiles} />} />
         </Route>
 
         <Route path='/problem'>
@@ -72,6 +68,8 @@ function App() {
           <Route index element={ViewBlog} />
           <Route path=':id' element={ViewSpecficBlog} />
         </Route>
+
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
 
